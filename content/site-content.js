ad1451428455378ef0a71e1e405346383080ddbb
duckdiveai <stephenstrout@me.com>
2026-05-28 13:26:58 -0700

    Update profile statement

--- content/site-content.js
+++ content/site-content.js
@@ -8,18 +8,16 @@ export const siteContent = {
     imdb: "https://www.imdb.com/name/nm0835200/?ref_=nv_sr_srsg_0_tt_0_nm_4_in_0_q_stephen%20B%20Strout",
   },
   profile: {
-    kicker: "Profile",
+    kicker: "",
     title: "",
-    intro:
-      "Stephen Strout is a documentary editor, producer, and director based in Los Angeles.",
+    intro: "",
     paragraphs: [
-      "Stephen is best known for editing and producing documentaries and feature films. He is a turn-around specialist, editing and creating deep emotions from minimal, creative assets. He made his directorial debut with the award-winning, feature documentary, <em>Revolution Green</em>. He won a Peabody Award for editing <em>Black Sky: The Race For Space</em> and a Telly Award for the feature, Regal Cinema trailer, Peter Berg's film, <em>Lone Survivor</em>. Throughout his career, his editing has been critically acclaimed, earning multiple Emmy nominations and one win, through collaborations with: Peter Berg, Rory Kennedy, Mark Bailey, Zackary Drucker, Maria Taylor, Christopher Cowen, Emmett and Brendan Malloy, and Academy Award winners Eric Simonson and Richard Trank.",
+      "Stephen Strout is a critically acclaimed editor and producer best known for mercurial collaborations transforming the most intimate human truths into universal experiences of emotional resonance.",
     ],
     recognition:
       "<strong>Peabody Award</strong>, 2005-<em>Black Sky: The Race For Space</em>\n<strong>Golden Cine Eagle</strong>, 2005-<em>Black Sky: The Race For Space</em>\n<strong>Telly Award</strong>, 2013-<em>Lone Survivor Trailer</em>\n<strong>Emmy Winner</strong>, 2016-<em>State Of Play-Fighting Chance</em>\n<strong>Sundance Premiere</strong>, 2024-<em>The Synanon Fix</em>\n<strong>Sundance Premiere</strong>, 2025-<em>Enigma</em>\n<strong>Emmy Nomination</strong>, 2026-<em>Enigma</em>\n<strong>Cinema Eye Nomination</strong>, 2026-<em>Enigma</em>\n<strong>GLAAD Outstanding Documentary Nomination</strong>, 2026-<em>Enigma</em>",
   },
-  projectsIntro:
-    "A selected list of films and series, with a behind the scenes story about the inception of each project.",
+  projectsIntro: "",
   contact: {
     heading: "so, you like what you see?",
     body: "",
@@ -293,7 +291,6 @@ export const siteContent = {
       posterAlt: "One-sheet artwork for Epic Bill",
       links: [],
     },
-
     {
       slug: "into-pitch-black",
       layout: "tall",
